15c91febb53b2553e0420efc820d0f51649db245
Jim Slattery <slattery.jim@gmail.com>
2025-02-12 23:57:45 -0500

    Copy in ResNet code from video
    https://github.com/foersterrobert/AlphaZeroFromScratch/blob/main/3.Model.ipynb
    
    Being added here:
    https://youtu.be/wuSQpLinRB4?si=deCYD9uN4G3ja7UU&t=6454

---
+++ resnet.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResBlock(nn.Module):
+    def __init__(self, num_hidden):
+        super().__init__()
+        self.conv1 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_hidden)
+        self.conv2 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(num_hidden)
+        
+    def forward(self, x):
+        residual = x
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.bn2(self.conv2(x))
+        x += residual
+        x = F.relu(x)
+        return x
+
+class ResNet(nn.Module):
+    def __init__(self, game, num_resBlocks, num_hidden):
+        super().__init__()
+        self.startBlock = nn.Sequential(
+            nn.Conv2d(3, num_hidden, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_hidden),
+            nn.ReLU()
+        )
+        
+        self.backBone = nn.ModuleList(
+            [ResBlock(num_hidden) for _ in range(num_resBlocks)]
+        )
+        
+        self.policyHead = nn.Sequential(
+            nn.Conv2d(num_hidden, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(32 * game.row_count * game.column_count, game.action_size)
+        )
+        
+        self.valueHead = nn.Sequential(
+            nn.Conv2d(num_hidden, 3, kernel_size=3, padding=1),
+            nn.BatchNorm2d(3),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(3 * game.row_count * game.column_count, 1),
+            nn.Tanh()
+        )
+        
+    def forward(self, x):
+        x = self.startBlock(x)
+        for resBlock in self.backBone:
+            x = resBlock(x)
+        policy = self.policyHead(x)
+        value = self.valueHead(x)
+        return policy, value
+
+
+if __name__ == '__main__':
+    # Define a dummy game class for testing purposes
+    class DummyGame:
+        def __init__(self):
+            self.row_count = 8
+            self.column_count = 8
+            self.action_size = 10
+
+    # Instantiate the dummy game and the model
+    game = DummyGame()
+    model = ResNet(game, num_resBlocks=5, num_hidden=64)
+    
+    # Create a dummy input tensor (batch size 1, 3 channels, 8x8 board)
+    dummy_input = torch.randn(1, 3, game.row_count, game.column_count)
+    
+    # Run the model
+    policy, value = model(dummy_input)
+    
+    print("Policy output (shape ", policy.shape, "): ", policy)
+    print("Value  output (shape ", value.shape,  "): ", value)
